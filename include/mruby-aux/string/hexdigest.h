e46dc43ebf226befd27e5516fa9756e091fe3adc
dearblue <dearblue@users.noreply.github.com>
2018-02-07 20:49:56 +0900

    Integer#to_s(16) みたいに uint64_t を16進数表記にする mrbx_str_new_as_hexdigest() 関数を追加

---
+++ include/mruby-aux/string/hexdigest.h
@@ -0,0 +1,31 @@
+#ifndef MRUBY_AUX_STRING_HEXDIGEST_H_
+#define MRUBY_AUX_STRING_HEXDIGEST_H_ 1
+
+#include "../string.h"
+
+static MRBX_UNUSED mrb_value
+mrbx_str_new_as_hexdigest(mrb_state *mrb, uint64_t n, int bytesize)
+{
+    if (bytesize < 1) {
+        bytesize = sizeof(size_t);
+    } else if (bytesize > sizeof(n)) {
+        bytesize = sizeof(n);
+    }
+
+    int off = bytesize * 8;
+    char str[bytesize * 2];
+    char *p = str;
+
+    for (; off > 0; off -= 4, p ++) {
+        uint8_t ch = (n >> (off - 4)) & 0x0f;
+        if (ch < 10) {
+            *p = ch + '0';
+        } else {
+            *p = ch + ('a' - 10);
+        }
+    }
+
+    return mrb_str_new(mrb, str, bytesize * 2);
+}
+
+#endif /* MRUBY_AUX_STRING_HEXDIGEST_H_ */
