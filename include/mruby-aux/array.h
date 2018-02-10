492b4d19a8d40f8b8521fa38b20b1d08524ba087
dearblue <dearblue@users.noreply.github.com>
2018-02-10 23:31:27 +0900

    C と C++ を問題のない範囲で統合 (一部)

--- include/mruby-aux/array.h
+++ include/mruby-aux/array.h
@@ -10,32 +10,9 @@
             ELEMENTOF(((const mrb_value []) { __VA_ARGS__ })),  \
             ((const mrb_value []) { __VA_ARGS__ }))             \
 
-#ifdef __cplusplus
+#define RArray(V)   _mrbx_ary_ptr(V)(mrb, (V))
 
-template <typename T> static inline struct RArray *_mrbx_ary_ptr(mrb_state *mrb, T v) { static_assert(sizeof(T) < 0, "wrong type"); }
-
-static inline struct RArray *
-_mrbx_ary_ptr(mrb_state *mrb, mrb_value v)
-{
-    if (mrb_nil_p(v)) {
-        return nullptr;
-    } else {
-        mrb_check_type(mrb, v, MRB_TT_ARRAY);
-        return mrb_ary_ptr(v);
-    }
-}
-
-static inline struct RArray *
-_mrbx_ary_ptr(mrb_state *mrb, struct RArray *p)
-{
-    return p;
-}
-
-#   define _mrbx_ary_ptr(V) _mrbx_ary_ptr
-
-#else
-
-static inline struct RArray *
+static MRBX_UNUSED struct RArray *
 _mrbx_ary_ptr(mrb_state *mrb, mrb_value v)
 {
     if (mrb_nil_p(v)) {
@@ -46,11 +23,20 @@ _mrbx_ary_ptr(mrb_state *mrb, mrb_value v)
     }
 }
 
-static inline struct RArray *
+static MRBX_UNUSED struct RArray *
 _mrbx_by_ary_ptr(mrb_state *mrb, struct RArray *p)
 {
     return p;
 }
+
+#ifdef __cplusplus
+
+template <typename T> static MRBX_UNUSED struct RArray *_mrbx_ary_ptr(mrb_state *mrb, T v) { static_assert(sizeof(T) < 0, "wrong type"); }
+static MRBX_UNUSED struct RArray *_mrbx_ary_ptr(mrb_state *mrb, struct RArray *p) { return _mrbx_by_ary_ptr(mrb, p); }
+
+#   define _mrbx_ary_ptr(V) _mrbx_ary_ptr
+
+#else
 
 #   define _mrbx_ary_ptr(V)                             \
         _Generic((V),                                   \
@@ -58,7 +44,5 @@ _mrbx_by_ary_ptr(mrb_state *mrb, struct RArray *p)
                  struct RArray *:   _mrbx_by_ary_ptr)   \
 
 #endif
-
-#define RArray(V)   _mrbx_ary_ptr(V)(mrb, (V))
 
 #endif /* MRUBY_AUX_ARRAY_H__ */
