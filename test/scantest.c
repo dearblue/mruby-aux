15e05b5c47c5ef999ad71402e4ae79a182128b59
dearblue <dearblue@users.noreply.github.com>
2018-02-11 21:43:49 +0900

    テストコードの追加

---
+++ test/scantest.c
@@ -0,0 +1,65 @@
+#include <mruby-aux/scanhash.h>
+#include <mruby.h>
+#include <mruby/numeric.h>
+#include <mruby/array.h>
+#include <mruby/variable.h>
+
+#define id_REQUIREMENT_KEYWORD mrb_intern_lit(mrb, "REQUIREMENT_KEYWORD")
+
+/* before mruby-1.4 */
+#ifndef ARY_LEN
+#   define ARY_LEN(A) ((A)->len)
+#endif
+
+/* before mruby-1.4 */
+#ifndef ARY_PTR
+#   define ARY_PTR(A) ((const mrb_value *)(A)->ptr)
+#endif
+
+static mrb_value
+get_default_value(mrb_state *mrb, struct RArray *ary, mrb_int index, mrb_value requirement)
+{
+    if (index < 0 || ARY_LEN(ary) < index - 1) {
+        return mrb_nil_value();
+    }
+
+    mrb_value v = ARY_PTR(ary)[index];
+
+    if (mrb_obj_eq(mrb, v, requirement)) {
+        return mrb_undef_value();
+    } else {
+        return v;
+    }
+}
+
+
+static mrb_value
+scan_static_keywords(mrb_state *mrb, mrb_value mAuxScanHashTest)
+{
+    mrb_value a, b, c, d;
+    mrb_value target, rest, defaults, block;
+    mrb_get_args(mrb, "ooA&", &target, &rest, &defaults, &block);
+
+    struct RArray *defaults_ary = RARRAY(defaults);
+    mrb_value req = mrb_const_get(mrb, mAuxScanHashTest, id_REQUIREMENT_KEYWORD);
+
+    rest = MRBX_SCANHASH(mrb, target, rest,
+            MRBX_SCANHASH_ARG("a", &a, get_default_value(mrb, defaults_ary, 0, req)),
+            MRBX_SCANHASH_ARG("b", &b, get_default_value(mrb, defaults_ary, 1, req)),
+            MRBX_SCANHASH_ARG("c", &c, get_default_value(mrb, defaults_ary, 2, req)),
+            MRBX_SCANHASH_ARG("d", &d, get_default_value(mrb, defaults_ary, 3, req)));
+
+    {
+        mrb_value args[] = { rest, a, b, c, d };
+
+        return mrb_yield_argv(mrb, block, sizeof(args) / sizeof(args[0]), args);
+    }
+}
+
+void
+mrb_mruby_aux_scanhash_gem_test(mrb_state *mrb)
+{
+    struct RClass *mAuxScanHashTest = mrb_define_module(mrb, "AuxScanHashTest");
+    mrb_define_class_method(mrb, mAuxScanHashTest, "scan_static_keywords", scan_static_keywords, MRB_ARGS_ANY());
+    mrb_const_set(mrb, mrb_obj_value(mAuxScanHashTest), id_REQUIREMENT_KEYWORD, mrb_obj_new(mrb, mrb->object_class, 0, NULL));
+}
---
+++ test/scantest.rb
@@ -0,0 +1,105 @@
+#!ruby
+
+assert "no keyword" do
+  AuxScanHashTest.scan_static_keywords(nil, nil, [nil, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal nil, rest
+    assert_equal nil, a
+    assert_equal 1, b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "no keyword with rest" do
+  rest0 = {}
+
+  AuxScanHashTest.scan_static_keywords(nil, rest0, [nil, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal rest0.object_id, rest.object_id
+    assert_equal ({}), rest
+    assert_equal nil, a
+    assert_equal 1, b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "no keyword with rest as true" do
+  AuxScanHashTest.scan_static_keywords(nil, true, [nil, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal ({}), rest
+    assert_equal nil, a
+    assert_equal 1, b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "no keyword with wrong rest type" do
+  rest0 = 1
+
+  assert_raise(TypeError) {
+    AuxScanHashTest.scan_static_keywords(nil, rest0, [nil, 1, 2.0, true])
+  }
+end
+
+assert "scan keyword" do
+  target = { a: 100, b: "abcdefg" }
+
+  AuxScanHashTest.scan_static_keywords(target, nil, [nil, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal nil, rest
+    assert_equal 100, a
+    assert_equal "abcdefg", b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "scan keyword (raise on rest)" do
+  target = { unnecessary: -1 }
+
+  assert_raise(ArgumentError) {
+    AuxScanHashTest.scan_static_keywords(target, nil, [nil, 1, 2.0, true])
+  }
+end
+
+assert "scan keyword (catch rest)" do
+  target = { unnecessary1: -1, unnecessary2: "ABCDEFG", a: 100, b: "abcdefg" }
+
+  AuxScanHashTest.scan_static_keywords(target, true, [nil, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal ({ unnecessary1: -1, unnecessary2: "ABCDEFG" }), rest
+    assert_equal 100, a
+    assert_equal "abcdefg", b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "wrong target" do
+  assert_raise(NoMethodError) {
+    AuxScanHashTest.scan_static_keywords(1, nil, [nil, 1, 2.0, true])
+  }
+end
+
+assert "requirement keyword" do
+  target = { a: 100 }
+
+  AuxScanHashTest.scan_static_keywords(target, nil, [AuxScanHashTest::REQUIREMENT_KEYWORD, 1, 2.0, true]) do |rest, a, b, c, d|
+    assert_equal 100, a
+    assert_equal 1, b
+    assert_equal 2.0, c
+    assert_equal true, d
+  end
+end
+
+assert "missing keyword from hash" do
+  target = { b: 5, c: 10, d: 15 }
+
+  assert_raise(ArgumentError) {
+    AuxScanHashTest.scan_static_keywords(target, nil, [AuxScanHashTest::REQUIREMENT_KEYWORD, 1, 2.0, true])
+  }
+end
+
+assert "missing keyword from nil" do
+  assert_raise(ArgumentError) {
+    AuxScanHashTest.scan_static_keywords(nil, nil, [AuxScanHashTest::REQUIREMENT_KEYWORD, 1, 2.0, true])
+  }
+end
