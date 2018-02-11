9260ba9b67ab69ea84c26095749291ca3819e90f
dearblue <dearblue@users.noreply.github.com>
2018-02-11 22:03:06 +0900

    テスト構築設定に C++17 相当 (C++1z) のための記述を追加

--- test_config.rb
+++ test_config.rb
@@ -6,8 +6,27 @@ MRuby::Build.new do |conf|
   enable_debug
   enable_test
 
+  cc.flags << "-Wall" << "-pedantic" << "-std=c11"
+
   gem core: "mruby-print"
   gem core: "mruby-bin-mirb"
   gem core: "mruby-bin-mruby"
-  gem "."
+  gem File.dirname(__FILE__)
+end
+
+MRuby::Build.new("host32++") do |conf|
+  toolchain :clang
+
+  conf.build_dir = "host32++"
+
+  enable_debug
+  enable_test
+  enable_cxx_abi
+
+  cc.flags << "-Wall" << "-pedantic" << "-std=c++1z"
+
+  gem core: "mruby-print"
+  gem core: "mruby-bin-mirb"
+  gem core: "mruby-bin-mruby"
+  gem File.dirname(__FILE__)
 end
