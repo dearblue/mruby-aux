b50777935f23261a28ab10c9fab0946b6e97e019
dearblue <dearblue@users.noreply.github.com>
2018-05-06 20:54:27 +0900

    mrbx_str_new_as_hexdigest() をグローバル関数化

--- include/mruby-aux/string/hexdigest.h
+++ include/mruby-aux/string/hexdigest.h
@@ -3,29 +3,6 @@
 
 #include "../string.h"
 
-MRBX_UNUSED static mrb_value
-mrbx_str_new_as_hexdigest(mrb_state *mrb, uint64_t n, int bytesize)
-{
-    if (bytesize < 1) {
-        bytesize = sizeof(size_t);
-    } else if (bytesize > sizeof(n)) {
-        bytesize = sizeof(n);
-    }
-
-    int off = bytesize * 8;
-    char str[bytesize * 2];
-    char *p = str;
-
-    for (; off > 0; off -= 4, p ++) {
-        uint8_t ch = (n >> (off - 4)) & 0x0f;
-        if (ch < 10) {
-            *p = ch + '0';
-        } else {
-            *p = ch + ('a' - 10);
-        }
-    }
-
-    return mrb_str_new(mrb, str, bytesize * 2);
-}
+mrb_value mrbx_str_new_as_hexdigest(mrb_state *mrb, uint64_t n, int bytesize);
 
 #endif /* MRUBY_AUX_STRING_HEXDIGEST_H_ */
---
+++ src/hexdigest.c
@@ -0,0 +1,26 @@
+#include <mruby-aux/string/hexdigest.h>
+
+mrb_value
+mrbx_str_new_as_hexdigest(mrb_state *mrb, uint64_t n, int bytesize)
+{
+    if (bytesize < 1) {
+        bytesize = sizeof(size_t);
+    } else if (bytesize > sizeof(n)) {
+        bytesize = sizeof(n);
+    }
+
+    int off = bytesize * 8;
+    char str[bytesize * 2];
+    char *p = str;
+
+    for (; off > 0; off -= 4, p ++) {
+        uint8_t ch = (n >> (off - 4)) & 0x0f;
+        if (ch < 10) {
+            *p = ch + '0';
+        } else {
+            *p = ch + ('a' - 10);
+        }
+    }
+
+    return mrb_str_new(mrb, str, bytesize * 2);
+}
