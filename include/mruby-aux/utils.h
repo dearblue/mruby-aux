53c2277375722a751102e9426076a9e0e30a7aea
dearblue <dearblue@users.noreply.github.com>
2018-09-04 22:02:34 +0900

    指定された mrb_data_type のオブジェクトを struct RData.dfree() する、mrbx_cleanup_data_objects() 関数を追加

--- include/mruby-aux/utils.h
+++ include/mruby-aux/utils.h
@@ -178,4 +178,6 @@ mrbx_error_arity(mrb_state *mrb, mrb_int argc, mrb_int min, mrb_int max)
     }
 }
 
+MRB_API void mrbx_cleanup_data_objects(mrb_state *mrb, int num, const struct mrb_data_type *types[]);
+
 #endif /* MRUBY_AUX_UTILS_H */
