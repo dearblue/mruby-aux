29a4903a574e17f3ae8cd8cc7c3e535a9ea973bf
dearblue <dearblue@users.noreply.github.com>
2018-09-07 00:30:55 +0900

    モジュールや特異クラスを取得できるように修正

--- include/mruby-aux/class.h
+++ include/mruby-aux/class.h
@@ -9,10 +9,16 @@
 MRBX_INLINE struct RClass *
 mrbx_class_ptr(mrb_state *mrb, mrb_value v)
 {
-    if (mrb_nil_p(v)) {
+    switch (mrb_type(v)) {
+    default:
+        mrb_raisef(mrb, E_TYPE_ERROR,
+                   "expect nil, module or class, but given %S",
+                   mrb_obj_value(mrb_obj_class(mrb, v)));
+    case MRB_TT_NIL:
         return NULL;
-    } else {
-        mrb_check_type(mrb, v, MRB_TT_CLASS);
+    case MRB_TT_MODULE:
+    case MRB_TT_CLASS:
+    case MRB_TT_SCLASS:
         return mrb_class_ptr(v);
     }
 }
@@ -51,7 +57,7 @@ mrbx_dig_class(mrb_state *mrb, struct RClass *c, size_t num, const char *names[]
     }
 
     for (; num > 0; num --, names ++) {
-        c = mrb_class_get_under(mrb, c, *names);
+        c = mrbx_class_ptr(mrb, mrb_const_get(mrb, mrb_obj_value(c), mrb_intern_cstr(names[0])));
     }
 
     return c;
