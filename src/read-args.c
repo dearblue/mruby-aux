01bb04371a6a722f322d8200e1caa75ab7736ba3
dearblue <dearblue@users.noreply.github.com>
2018-12-02 11:54:02 +0900

    mrbx_get_read_args() が常に新しい buf を生成していたため修正

--- src/read-args.c
+++ src/read-args.c
@@ -34,7 +34,7 @@ mrbx_get_read_args(mrb_state *mrb, ssize_t *size, struct RString **dest)
             }
         }
 
-        *dest = (mrb_nil_p(bufv) ? mrbx_str_ptr(mrb, bufv) : NULL);
+        *dest = (mrb_nil_p(bufv) ? NULL : RSTRING(bufv));
     }
 
     *dest = mrbx_str_force_recycle(mrb, *dest, (*size < 0 ? 0 : *size));
