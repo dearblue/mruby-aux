128f069d263fafbf5808b02f1f8f7b951d7859a6
dearblue <dearblue@users.noreply.github.com>
2018-12-19 23:34:16 +0900

    mruby-aux/array.h のテストを追加

---
+++ test/array.c
@@ -0,0 +1,73 @@
+#include <mruby-aux/array.h>
+
+#if defined(__clang__)
+# pragma GCC diagnostic ignored "-Wgnu-empty-initializer"
+# pragma GCC diagnostic ignored "-Wzero-length-array"
+#endif
+
+static mrb_value
+test_tuple_0(mrb_state *mrb, mrb_value self)
+{
+    return MRBX_TUPLE();
+}
+
+#if defined(__clang__)
+# pragma GCC diagnostic warning "-Wgnu-empty-initializer"
+# pragma GCC diagnostic warning "-Wzero-length-array"
+#endif
+
+static mrb_value
+test_tuple_3(mrb_state *mrb, mrb_value self)
+{
+    return MRBX_TUPLE(mrb_fixnum_value(1), mrb_fixnum_value(2), mrb_fixnum_value(3));
+}
+
+static mrb_value
+test_tuple_5(mrb_state *mrb, mrb_value self)
+{
+    return MRBX_TUPLE(mrb_fixnum_value(1),
+                      mrb_fixnum_value(2),
+                      mrb_fixnum_value(3),
+                      mrb_fixnum_value(4),
+                      mrb_fixnum_value(5));
+}
+
+static mrb_value
+test_ary_ptr(mrb_state *mrb, mrb_value self)
+{
+    mrb_value obj;
+    mrb_get_args(mrb, "A!", &obj);
+
+    struct RArray *ary = mrbx_ary_ptr(mrb, obj);
+
+    if (ary) {
+        return mrb_obj_value(ary);
+    } else {
+        return mrb_nil_value();
+    }
+}
+
+static mrb_value
+test_ary_ptr_double(mrb_state *mrb, mrb_value self)
+{
+    mrb_value obj;
+    mrb_get_args(mrb, "A!", &obj);
+
+    struct RArray *ary = mrbx_ary_ptr(mrb, mrbx_ary_ptr(mrb, obj));
+
+    if (ary) {
+        return mrb_obj_value(ary);
+    } else {
+        return mrb_nil_value();
+    }
+}
+
+void
+mruby_aux_test_array_init(mrb_state *mrb, struct RClass *test)
+{
+    mrb_define_class_method(mrb, test, "tuple_0",         test_tuple_0,        MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, test, "tuple_3",         test_tuple_3,        MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, test, "tuple_5",         test_tuple_5,        MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, test, "ary_ptr",         test_ary_ptr,        MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, test, "ary_ptr_double ", test_ary_ptr_double, MRB_ARGS_ANY());
+}
--- test/aux-test.c
+++ test/aux-test.c
@@ -7,6 +7,7 @@
 #include <mruby/array.h>
 #include <mruby/variable.h>
 
+void mruby_aux_test_array_init(mrb_state *mrb, struct RClass *test);
 void mruby_aux_test_fakedin_init(mrb_state *mrb, struct RClass *test);
 void mruby_aux_test_scanhash_init(mrb_state *mrb, struct RClass *test);
 void mruby_aux_test_string_init(mrb_state *mrb, struct RClass *test);
@@ -16,6 +17,7 @@ mrb_mruby_aux_gem_test(MRB)
 {
     struct RClass *mAuxTest = mrb_define_module(mrb, "AuxTest");
 
+    mruby_aux_test_array_init(mrb, mAuxTest);
     mruby_aux_test_fakedin_init(mrb, mAuxTest);
     mruby_aux_test_scanhash_init(mrb, mAuxTest);
     mruby_aux_test_string_init(mrb, mAuxTest);
