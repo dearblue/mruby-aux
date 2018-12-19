b97f0555ca6b3c5b7a5898d41d02558075f32e31
dearblue <dearblue@users.noreply.github.com>
2018-12-19 23:34:45 +0900

    ヘッダファイルを <mruby.h> のみとなるように修正

--- test/aux-test.c
+++ test/aux-test.c
@@ -1,11 +1,4 @@
 #include <mruby.h>
-#include <mruby-aux.h>
-#include <mruby-aux/string.h>
-#include <mruby-aux/fakedin.h>
-#include <mruby-aux/scanhash.h>
-#include <mruby/numeric.h>
-#include <mruby/array.h>
-#include <mruby/variable.h>
 
 void mruby_aux_test_array_init(mrb_state *mrb, struct RClass *test);
 void mruby_aux_test_fakedin_init(mrb_state *mrb, struct RClass *test);
@@ -13,7 +6,7 @@ void mruby_aux_test_scanhash_init(mrb_state *mrb, struct RClass *test);
 void mruby_aux_test_string_init(mrb_state *mrb, struct RClass *test);
 
 void
-mrb_mruby_aux_gem_test(MRB)
+mrb_mruby_aux_gem_test(mrb_state *mrb)
 {
     struct RClass *mAuxTest = mrb_define_module(mrb, "AuxTest");
 
