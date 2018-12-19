c944739b0672fcadd302c2d3e3d2c1ec743b2d24
dearblue <dearblue@users.noreply.github.com>
2018-12-19 23:23:14 +0900

    mrbx_fakedin API のテストを分離

--- test/aux-test.c
+++ test/aux-test.c
@@ -17,76 +17,6 @@ test_str_drop(MRB, VALUE self)
     return VALUE(mrbx_str_drop(mrb, RSTRING(str), off, size));
 }
 
-static const mrb_data_type fakedin_type = {
-    "fakedin@mruby-aux",
-    mrb_free,
-};
-
-static struct mrbx_fakedin *
-check_fakedin(MRB, VALUE obj, struct mrbx_fakedin *ptr)
-{
-    if (!ptr) {
-        mrb_raisef(mrb, E_RUNTIME_ERROR,
-                   "invalid initialization - %S",
-                   obj);
-    }
-
-    return ptr;
-}
-
-static struct mrbx_fakedin *get_fakedinp(MRB, VALUE obj) { return (struct mrbx_fakedin *)mrb_data_get_ptr(mrb, obj, &fakedin_type); }
-static struct mrbx_fakedin *get_fakedin(MRB, VALUE obj) { return check_fakedin(mrb, obj, get_fakedinp(mrb, obj)); }
-
-static VALUE
-fakedin_initialize(MRB, VALUE self)
-{
-    VALUE stream;
-    mrb_get_args(mrb, "o", &stream);
-
-    if (DATA_PTR(self) || DATA_TYPE(self)) {
-        mrb_raisef(mrb, E_RUNTIME_ERROR,
-                   "already initialized - %S",
-                   self);
-    }
-
-    struct mrbx_fakedin *p = (struct mrbx_fakedin *)mrb_malloc(mrb, sizeof(struct mrbx_fakedin));
-    mrbx_fakedin_setup(mrb, self, p, stream);
-    mrb_data_init(self, p, &fakedin_type);
-
-    return self;
-}
-
-static VALUE
-fakedin_read(MRB, VALUE self)
-{
-    mrb_int size;
-    mrb_get_args(mrb, "i", &size);
-
-    const char *buf;
-    size = mrbx_fakedin_read(mrb, self, get_fakedin(mrb, self), &buf, size);
-    if (size < 0) {
-        return Qnil;
-    }
-
-    return mrb_str_new(mrb, buf, size);
-}
-
-static VALUE
-fakedin_eof(MRB, VALUE self)
-{
-    mrb_get_args(mrb, "");
-
-    return mrb_bool_value(mrbx_fakedin_eof(get_fakedin(mrb, self)));
-}
-
-static VALUE
-fakedin_tell(MRB, VALUE self)
-{
-    mrb_get_args(mrb, "");
-
-    return mrb_fixnum_value(mrbx_fakedin_total_in(get_fakedin(mrb, self)));
-}
-
 #define id_REQUIREMENT_KEYWORD mrb_intern_lit(mrb, "REQUIREMENT_KEYWORD")
 
 static mrb_value
@@ -129,6 +59,8 @@ scan_static_keywords(mrb_state *mrb, mrb_value mAuxScanHashTest)
     }
 }
 
+void mruby_aux_test_fakedin_init(mrb_state *mrb, struct RClass *test);
+
 void
 mrb_mruby_aux_gem_test(MRB)
 {
@@ -136,14 +68,9 @@ mrb_mruby_aux_gem_test(MRB)
 
     mrb_define_class_method(mrb, mAuxTest, "str_drop", test_str_drop, MRB_ARGS_REQ(3));
 
-    struct RClass *cFakedIn = mrb_define_class_under(mrb, mAuxTest, "FakedIn", mrb->object_class);
-    MRB_SET_INSTANCE_TT(cFakedIn, MRB_TT_DATA);
-    mrb_define_method(mrb, cFakedIn, "initialize", fakedin_initialize, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, cFakedIn, "read", fakedin_read, MRB_ARGS_REQ(1));
-    mrb_define_method(mrb, cFakedIn, "eof?", fakedin_eof, MRB_ARGS_NONE());
-    mrb_define_method(mrb, cFakedIn, "tell", fakedin_tell, MRB_ARGS_NONE());
-
     struct RClass *mAuxScanHashTest = mrb_define_module(mrb, "AuxScanHashTest");
     mrb_define_class_method(mrb, mAuxScanHashTest, "scan_static_keywords", scan_static_keywords, MRB_ARGS_ANY());
     mrb_const_set(mrb, mrb_obj_value(mAuxScanHashTest), id_REQUIREMENT_KEYWORD, mrb_obj_new(mrb, mrb->object_class, 0, NULL));
+
+    mruby_aux_test_fakedin_init(mrb, mAuxTest);
 }
