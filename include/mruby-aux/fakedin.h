35e09c4818cae47df65679ab0c6076e44e0444a7
dearblue <dearblue@users.noreply.github.com>
2019-08-25 18:53:29 +0900

    `mrbx_fakedin` API を再実装
    構造体を隠蔽して mruby オブジェクトとして扱うようにしました。

--- include/mruby-aux/fakedin.h
+++ include/mruby-aux/fakedin.h
@@ -6,48 +6,10 @@
 
 MRB_BEGIN_DECL
 
-struct mrbx_fakedin
-{
-    /* 読み込み対象 */
-    mrb_value stream;
-
-    union {
-        struct {
-            /* 現在の読み込んだ総バイト量 */
-            int64_t off;
-
-            int64_t selector;
-        };
-
-        struct {
-            int64_t off;
-
-            const int64_t constant_negative;
-
-            /*
-             * stream の実体
-             */
-            struct RString *buf;
-        } string;
-
-        struct {
-            const int64_t total;
-
-            /* buf 中におけるバイト位置 */
-            int64_t off;
-
-            /*
-             * stream を段階的に読み込むデータ領域
-             */
-            struct RString *buf;
-        } read;
-    };
-};
-
-MRB_API void mrbx_fakedin_setup(mrb_state *mrb, mrb_value owner, struct mrbx_fakedin *input, mrb_value stream);
-MRB_API mrb_int mrbx_fakedin_read(mrb_state *mrb, mrb_value owner, struct mrbx_fakedin *input, const char **buf, mrb_int size);
-MRB_API mrb_bool mrbx_fakedin_eof(struct mrbx_fakedin *input);
-MRB_API int64_t mrbx_fakedin_total_in(struct mrbx_fakedin *input);
+MRB_API mrb_value mrbx_fakedin_new(mrb_state *mrb, mrb_value stream);
+MRB_API mrb_int mrbx_fakedin_read(mrb_state *mrb, mrb_value fakedin, const char **buf, mrb_int size);
+MRB_API mrb_bool mrbx_fakedin_eof(mrb_state *mrb, mrb_value fakedin);
+MRB_API int64_t mrbx_fakedin_total_in(mrb_state *mrb, mrb_value fakedin);
 
 MRB_END_DECL
 
