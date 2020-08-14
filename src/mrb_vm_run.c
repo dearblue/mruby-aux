4ad10a502e40652657721fb0c259b9d9d7e54ed4
dearblue <dearblue@users.osdn.me>
2020-08-14 10:02:22 +0900

    `MRB_RELEASE_NO` は `MRUBY_RELEASE_NO` の間違い

--- src/mrb_vm_run.c
+++ src/mrb_vm_run.c
@@ -1,6 +1,6 @@
 #include <mruby-aux/compat/mruby.h>
 
-#if MRB_RELEASE_NO < 10300
+#if MRUBY_RELEASE_NO < 10300
 MRB_API mrb_value
 mrb_vm_run(mrb_state *mrb, struct RProc *proc, mrb_value self, unsigned int stack_keep)
 {
