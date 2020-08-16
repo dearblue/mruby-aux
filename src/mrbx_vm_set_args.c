676e95c642ab3dadd98fd2bdaf5d830cb9ab31df
dearblue <dearblue@users.osdn.me>
2020-08-16 11:10:39 +0900

    `mrbx_vm_set_args()` の改善
    - ブロックオブジェクトのコピーを忘れていたため、行うように修正します。
    - 処理の共通化を推し進めます。

--- src/mrbx_vm_set_args.c
+++ src/mrbx_vm_set_args.c
@@ -1,6 +1,7 @@
 #include <mruby-aux/vmext.h>
 #include <mruby-aux/compat/proc.h>
 #include <mruby/array.h>
+#include <stdlib.h>
 
 #ifndef MRB_FUNCALL_ARGC_MAX
 # define MRB_FUNCALL_ARGC_MAX 16
@@ -9,31 +10,29 @@
 int
 mrbx_vm_set_args(mrb_state *mrb, mrb_callinfo *ci, mrb_value recv, int argc, const mrb_value argv[], mrb_value block, mrb_sym mid)
 {
-  int argc_mod = argc + (mid != 0 ? 1 : 0);
-  mrb_value *st = mrbx_vm_stack_extend(mrb, (argc_mod > MRB_FUNCALL_ARGC_MAX ? 3 : argc_mod));
+  int needblock = 1;
+  int argc_mod = (mid != 0 ? 1 : 0) + argc;
+  mrb_value *st = mrbx_vm_stack_extend(mrb, (argc_mod > MRB_FUNCALL_ARGC_MAX ? 3 : argc_mod + needblock));
 
   *st ++ = recv;
 
+  mrb_value *ap;
   if (argc_mod > MRB_FUNCALL_ARGC_MAX) {
     ci->argc = -1;
-    *st = mrb_ary_new_from_values(mrb, argc_mod, argv);
-
-    if (mid != 0) {
-      mrb_ary_unshift(mrb, *st, mrb_symbol_value(mid));
-    }
-
+    *st = mrb_ary_new_capa(mrb, argc_mod);
+    ap = RARRAY_PTR((*st));
     st ++;
   } else {
     ci->argc = argc_mod;
-
-    if (mid != 0) {
-      *st ++ = mrb_symbol_value(mid);
-    }
-
-    for (; argc > 0; argc --) {
-      *st ++ = *argv ++;
-    }
+    ap = st;
+    st += argc_mod;
   }
+
+  if (mid != 0) {
+    *ap ++ = mrb_symbol_value(mid);
+  }
+
+  memmove(ap, argv, sizeof(mrb_value) * argc);
 
   *st ++ = block;
 
