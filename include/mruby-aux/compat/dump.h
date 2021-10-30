65c52632dc688c7c6f4ee93741b4922b76a66463
dearblue <dearblue@users.osdn.me>
2021-10-30 13:15:57 +0900

    `mrb_true_p` の有無でバージョンの確認を補完しない

--- include/mruby-aux/compat/dump.h
+++ include/mruby-aux/compat/dump.h
@@ -4,7 +4,7 @@
 #include <mruby.h>
 #include <mruby/dump.h>
 
-#if MRUBY_RELEASE_NO < 20002 && !defined(mrb_true_p)
+#if MRUBY_RELEASE_NO < 20100
 MRB_API mrb_irep *mrb_read_irep_buf(mrb_state *mrb, const void *buf, size_t bufsize);
 #endif
 
--- include/mruby-aux/compat/irep.h
+++ include/mruby-aux/compat/irep.h
@@ -4,7 +4,7 @@
 #include <mruby.h>
 #include <mruby/irep.h>
 
-#if MRUBY_RELEASE_NO < 20002 && !defined(mrb_true_p)
+#if MRUBY_RELEASE_NO < 20100
 MRB_API mrb_value mrb_load_irep_buf(mrb_state *mrb, const void *buf, size_t bufsize);
 MRB_API mrb_value mrb_load_irep_buf_cxt(mrb_state *mrb, const void *buf, size_t bufsize, mrbc_context *ctx);
 #endif
