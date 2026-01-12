3a61a0fe387eadec00961ad57b401fbd140faa2e
dearblue <dearblue@gmx.com>
2026-01-12 22:59:59 +0900

    補助マクロ関数 `MRBX_ENSURE()` を追加

--- include/mruby-aux/error.h
+++ include/mruby-aux/error.h
@@ -3,6 +3,8 @@
 
 #include "common.h"
 #include <mruby.h>
+#include <mruby-aux/compat/error.h>
+#include <mruby-aux/pp.h>
 
 MRB_BEGIN_DECL
 
@@ -13,6 +15,34 @@ mrbx_protect_exceptions(
         mrb_func_t rescue, mrb_value r_data,
         mrb_func_t ensure, mrb_value e_data,
         mrb_int len, struct RClass *classes[]);
+
+/**
+ *  引数 `FUNC` を呼び出し、その結果を変数 `RESULT_VAR` に代入します。
+ *
+ *  `FUNC` の実行による大域ジャンプの発生にかかわらず、指定されたブロックが実行されます。
+ *
+ *  大域ジャンプが発生しなければ、コードの書かれた順序にそってブロックの外側が実行されます。
+ *  大域ジャンプが発生すれば、ブロックが終わると大域ジャンプが再開され、ブロックの外側は実行されません。
+ *
+ *      mrb_value result;
+ *      MRBX_ENSURE(mrb, result, func, userdata) {
+ *        // func が正常に制御を返しても、例外などの帯域ジャンプが発生しても、常に実行される。
+ *        // Ruby の ensure ブロックと同等。
+ *
+ *        ...
+ *
+ *        // 例外などの大域ジャンプが発生している場合、このブロックが普通に終わると大域ジャンプが再開される。
+ *        // もし大域ジャンプを無視したい場合、`mrb->exc = NULL; break;` を行うとよい。
+ *      }
+ */
+#define MRBX_ENSURE(MRB, RESULT_VAR, FUNC, DATA) \
+        for (mrb_bool MRBX_TMPVAR(_break_) = FALSE; \
+             !MRBX_TMPVAR(_break_) && \
+                (((RESULT_VAR) = mrb_protect_error(MRB, (FUNC), (DATA), &MRBX_TMPVAR(_break_))), \
+                 ((MRB)->exc = (MRBX_TMPVAR(_break_) ? mrb_obj_ptr((RESULT_VAR)) : NULL)), \
+                 TRUE); \
+             (void)(MRBX_TMPVAR(_break_) && (mrb_exc_raise(mrb, RESULT_VAR), TRUE)), \
+                MRBX_TMPVAR(_break_) = TRUE)
 
 MRB_END_DECL
 
